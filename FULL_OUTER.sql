0ecbb588da0805f63ed2cf10bb9b2dc2629bd06e
Ohad129 <2000ohad@gmail.com>
2026-05-07 20:03:21 +0300

    Create FULL_OUTER.sql

---
+++ FULL_OUTER.sql
@@ -0,0 +1,25 @@
+-- 1. Write a FULL OUTER JOIN to show all drivers and all cars in one result set
+
+SELECT
+  COALESCE(d.name,'— no driver assigned —') AS driver_name,
+  coalesce(c.model, '- no car assigned -') AS car_model
+FROM   drivers d
+FULL OUTER JOIN cars c ON d.car_id = c.id
+-- 2. From the full-outer result, find only the unmatched rows on EITHER side — drivers with no car AND cars with no driver — add a WHERE clause
+WHERE c.id IS NULL OR d.id IS NULL;
+
+-- 3. Use COALESCE to replace NULL car prices with 0 for drivers who have no car assigned
+
+SELECT
+  COALESCE(d.name,'— no driver assigned —') AS driver_name,
+  coalesce(c.price, 0) AS car_price
+FROM   drivers d
+FULL OUTER JOIN cars c ON d.car_id = c.id
+
+-- should i use left join here? if so then here's the code:
+
+-- LEFT JOIN cars c ON d.car_id = c.id
+
+
+-- 4. Which single join type lets you find drivers WITHOUT a car AND cars WITHOUT a driver in one query?
+ -- The answer is: FULL OUTER JOIN, because its the ONLY one that lets you take unmatched rows from both tables, after you can filter it with where (q2).
